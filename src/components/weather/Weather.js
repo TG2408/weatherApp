920283acd5169667e3d1de5753bf48d5dc7adca2
TG2408 <tanmaygupta2408@gmail.com>
2021-10-23 17:34:28 +0530

    styling done

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from "react";
 import styles from './style.module.css'
 import './weather-icons.min.css'
 
+
 const Weather = () => {
     // fetch("https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=7adf3a153b451eea706144e62dab30a1")
     // .then(x => x.text())
@@ -45,11 +46,52 @@ const Weather = () => {
                     </button>
                 </div>
             </div>
-            <article id="temp">
-                <div>
+            
+            {/* OUR TEMP CARD */}
+            <article className={styles.widget}>
+                <div className={styles.weatherIcon}>
                     <i className="wi wi-day-sunny"></i>
                 </div>
+                
+                <div className={styles.basicInfo}>
+                    <div className={styles.weatherInfo}>
+                        <span>25.5&deg;</span>
+                    </div>
+                    <div className={styles.weatherConditions}>
+                        sunny    
+                    </div>
+                    <div className={styles.place}>
+                        maharashtra
+                    </div>
+                    <div className={styles.date}>
+                        {new Date().toLocaleString()}
+                    </div>
+                </div>
+
+                {/* Our 4 devided section */}
+                <div className={styles.extraInfo}>
+                    <div className={styles.sunset}>
+                        <i className="wi wi-day-sunny"></i>    
+                        <i>15:26 PM Sunset</i>
+                    </div>
+                    <div className={styles.humidity}>
+                        <i className="wi wi-day-sunny"></i>    
+                        <i>90% Humidity</i>
+                    </div>
+                    <div className={styles.pressure}>
+                        <i className="wi wi-day-sunny"></i>    
+                        <i>60% pressure</i>
+                    </div>
+                    <div className={styles.wind}> 
+                        <i className="wi wi-day-sunny"></i>    
+                        <i>50 KM/H Speed</i>
+                    </div>
+                </div>
             </article>
+
+
+            
+
         </>
     )
 }
